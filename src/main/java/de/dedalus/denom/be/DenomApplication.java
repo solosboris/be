03d0977cecb1c16bbe0cf4dd4ba309a0a13efe9f
solosboris <solosboris@gmail.com>
2025-09-22 16:56:47 +0200

    denombe: created initial project's structure

---
+++ src/main/java/de/dedalus/denom/be/DenomApplication.java
@@ -0,0 +1,15 @@
+package de.dedalus.denom.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				DenomApplication.class, args
+		);
+	}
+
+}
---
+++ src/test/java/de/dedalus/denom/be/DenomApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dedalus.denom.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
